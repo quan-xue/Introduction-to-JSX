ec101c1c3f4a87f49b0273105a4d47f9b74104bc
Quan Xue <33763102+quan-xue@users.noreply.github.com>
2020-10-03 08:34:22 +0800

    End of lesson Embed element within div, comparison with vanilla JS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(); //what to show, where to show
+ReactDOM.render(
+  <div>
+    <h1> Hello World! </h1>
+    <p>This is a paragraph </p>
+  </div>,
+  document.getElementById("root")
+); //what to show, where to show
+//JSX files are created
+
+//comparison with vanilla JS, which is really long
+var h1 = document.createElement("h1");
+h1.innerHTML = "Hello World!";
+document.getElementById("root").appendChild(h1);
